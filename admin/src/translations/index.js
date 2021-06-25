b35d7e9c9400fa8de02c1ecc7fe7bc1dcb1c3d26
Sara Sanchez <saralesan@hotmail.com>
2021-06-25 02:05:17 -0500

    setting tanslations

---
+++ admin/src/translations/index.js
@@ -0,0 +1,14 @@
+import en from './en.json';
+import es from './es.json';
+
+const trads = {
+  en,
+  es,
+};
+
+export const languageNativeNames = {
+  en: 'English',
+  es: 'Español',
+};
+
+export default trads;
