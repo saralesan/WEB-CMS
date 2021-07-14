398c88f6fabe295fd0cb097c778cf625e94f1dcd
saralesan <saralesan@hotmail.com>
2021-07-14 03:36:58 +0000

    updated fields

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'cc90561c405501e243163df2d6536cff'),
