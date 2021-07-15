4f8d5403a1b3808f4e88132ef8e49ccc9cd74798
Sara Sanchez <saralesan@hotmail.com>
2021-07-15 00:08:36 -0500

    ontact-us email

--- api/contactenos/models/contactenos.js
+++ api/contactenos/models/contactenos.js
@@ -5,4 +5,21 @@
  * to customize this model
  */
 
-module.exports = {};
+ module.exports = {
+  lifecycles: {
+      async  afterCreate(result, data) {
+
+          await strapi.plugins['email'].services.email.send({
+            to: 'saralesan@hotmail.com',
+            subject: 'Has recibido un mensaje en Web Event Box',
+            html: `<p>Un usuario a escrito en <b>Web Event Box</b></p>
+            <ul><li>Nombre: ` + data.name + `</li>
+            <li>Email: ` + data.email + `</li>
+            <li>Teléfono: ` + data.phone + `</li>
+            <li>Empresa: ` + data.company + `</li>
+            <li>Mensaje: ` + data.comment + `</li></ul>`,
+          });
+
+      },
+  },
+};
